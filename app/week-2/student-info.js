3dac1763e67668fb5e49387a5a663a28b5d65f9d
Samarth Bhasin <78861159+bhasinsamarth@users.noreply.github.com>
2025-01-28 16:23:46 -0700

    initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+function StudentInfo() {
+    return (
+        <div>
+            <h1>Student Information</h1>
+            <p>Name: Samarth Bhasin</p>
+            <Link href=""></Link>   
+
+        </div>
+    );
+}
