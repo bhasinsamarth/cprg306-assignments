09d54818a1e56cf3e311aece7b67176263012784
Samarth Bhasin <78861159+bhasinsamarth@users.noreply.github.com>
2025-02-25 23:08:31 -0700

    week-5

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Page() {
             <p>Click on the link to view the Week-2 assignment: <Link href="./week-2" style={{fontStyle: "italic"}}>Week 2</Link></p>
             <p>Click on the link to view the Week-3 assignment: <Link href="./week-3" style={{fontStyle: "italic"}}>Week 3</Link></p>
             <p>Click on the link to view the Week-4 assignment: <Link href="./week-4" style={{fontStyle: "italic"}}>Week 4</Link></p>
+            <p>Click on the link to view the Week-5 assignment: <Link href="./week-5" style={{fontStyle: "italic"}}>Week 5</Link></p>
       
         </main>
     );
