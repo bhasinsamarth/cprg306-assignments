840f4c06f0aca127853bcc1214c05cbd95cda30b
Samarth Bhasin <78861159+bhasinsamarth@users.noreply.github.com>
2025-03-02 12:38:09 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Page() {
     return (
         <main className='bg-black text-white w-screen h-screen pt-3'>
-            <div className='bg-gray-800 w-max h-screen p-5 rounded-md align-middle m-auto'>
+            <div className='bg-gray-800 w-max h-max p-5 rounded-md align-middle m-auto'>
                 <h1 className='text-3xl font-bold underline text-center'>CPRG 306: Web Development 2 - Assignments</h1>
                 <p>Links to the weekly progress:</p>
                 <p>Week-2 assignment: <Link href="./week-2" className='italic hover:underline hover:text-blue-400'>Week 2</Link></p>
